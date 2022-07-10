08662d821dbaf4c2ee0e37cd8f3fd7b6b50edffa
beesetti saikumar <101691634+saikumarbeesetti1@users.noreply.github.com>
2022-07-10 22:54:49 +0530

    Update and rename assignmet_1_q2.py to assignmet_1_quetion  1 and 2.py

--- assignmet_1_q2.py
+++
@@ -1,3 +0,0 @@
-
-Cluster similar Houses  link url : "https://colab.research.google.com/drive/1GSvOKTbM7Imfw5AOmOifixVBrogcPeea?usp=sharing"
-
---
+++ assignmet_1_quetion  1 and 2.py
@@ -0,0 +1,8 @@
+
+
+
+
+
+
+Cluster similar Houses  collab link url : "https://colab.research.google.com/drive/1GSvOKTbM7Imfw5AOmOifixVBrogcPeea?usp=sharing"
+
