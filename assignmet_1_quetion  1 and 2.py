7ec3feee7aa23c3b4f2a97842784aff81177964a
beesetti saikumar <101691634+saikumarbeesetti1@users.noreply.github.com>
2022-07-10 22:59:30 +0530

    Update assignmet_1_quetion  1 and 2.py

--- assignmet_1_quetion  1 and 2.py
+++ assignmet_1_quetion  1 and 2.py
@@ -1,8 +1,8 @@
+Quetion_1 - Use KNearestNeighbourRegressor to Predict Price of House : "
 
 
 
 
 
-
-Cluster similar Houses  collab link url : "https://colab.research.google.com/drive/1GSvOKTbM7Imfw5AOmOifixVBrogcPeea?usp=sharing"
+Quetion_2 - Cluster similar Houses  collab link url : "https://colab.research.google.com/drive/1GSvOKTbM7Imfw5AOmOifixVBrogcPeea?usp=sharing"
 
