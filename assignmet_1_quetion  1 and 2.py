74ead0c76d86f02ede9142993ae82e89a031b191
beesetti saikumar <101691634+saikumarbeesetti1@users.noreply.github.com>
2022-07-11 00:05:40 +0530

    Delete assignmet_1_quetion  1 and 2.py

--- assignmet_1_quetion  1 and 2.py
+++
@@ -1,8 +0,0 @@
-Quetion_1 - Use KNearestNeighbourRegressor to Predict Price of House collab link url  : "https://colab.research.google.com/drive/1xgiPd38tqehDnetZUlLpELU9lVmO1NSB?usp=sharing"
-
-
-
-
-
-Quetion_2 - Cluster similar Houses  collab link url : "https://colab.research.google.com/drive/1GSvOKTbM7Imfw5AOmOifixVBrogcPeea?usp=sharing"
-
